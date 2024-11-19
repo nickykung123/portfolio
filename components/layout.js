3d6f7c9469bac1c18cdc878a626c1a31549378c8
nickykung123 <Email>
2024-11-19 15:02:01 +0700

    Kittisak se66

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Kittisak';
+const name = 'Kittisak SE66';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
